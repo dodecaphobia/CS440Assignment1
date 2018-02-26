3cab62dcaaffafd2f0f7f5f3083036e2c2cc8606
dodecaphobia <cba30@scarletmail.rutgers.edu>
2018-02-25 22:44:22 -0500

    heap now functional, create Heap using Heap()
    Example: x = Heap()

--- heap.py
+++ heap.py
@@ -2,40 +2,33 @@
 class Heap(object):
 	l = []
 
-	# for non-heaps, takes it and heapifies
-	def __init__(nonHeap):
-		l = nonHeap
-		heapify()
-
 	# adds an element to the bottom of the heap
-	def insert(x):
-		l.append(x)
-		loc = len(x) - 1
+	def insert(self, x):
+		self.l.append(x)
+		loc = len(self.l) - 1
 		# while not at the top of the heap and greater than the parent, swap
-		while loc != 0 and l[loc] > l[(int)(loc // 2)]:
-			l[loc], l[(int)(loc // 2)] = l[(int)(loc // 2)], l[loc]
+		while loc != 0 and self.l[loc] < self.l[(int)(loc // 2)]:
+			self.l[loc], self.l[(int)(loc // 2)] = self.l[(int)(loc // 2)], self.l[loc]
 			loc = (int)(loc // 2)
 
 	# removes the top element and sifts upward as necessary
-	def extract():
+	def extract(self):
+		if len(self.l) == 0:
+			return -1
 		# retrieves top element
-		x = l[0]
+		x = self.l[0]
+		if len(self.l) == 1:
+			x = self.l.pop()
+			return x
 		# take lowest element and place at top
 		loc = 0
-		l[0] = l.pop()
-		# sift down while causing problems
-		while max(l[loc], l[loc * 2], l[loc * 2 + 1]) != l[loc]:
-			if l[loc * 2] > l[loc * 2 + 1]:
-				l[loc], l[loc * 2] = l[loc * 2], l[loc]
+		self.l[0] = self.l.pop()
+		# sift down while less than values below it
+		while loc * 2 < len(self.l) and min(self.l[loc], self.l[loc * 2]) != self.l[loc] and (loc * 2 + 1 < len(self.l) or min(self.l[loc], self.l[loc * 2 + 1]) != self.l[loc]):
+			if self.l[loc * 2] < self.l[loc * 2 + 1]:
+				self.l[loc], self.l[loc * 2] = self.l[loc * 2], self.l[loc]
 				loc *= 2
 			else:
-				l[loc], l[loc * 2 + 1] = l[loc * 2 + 1], l[loc]
+				self.l[loc], self.l[loc * 2 + 1] = self.l[loc * 2 + 1], self.l[loc]
 				loc = loc * 2 + 1
 		return x
-
-	# turns a non-heapified list into a heap
-	def heapify():
-		temp = l
-		l = []
-		for i in temp:
-			insert(i)
